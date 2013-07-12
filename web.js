e58efe1cca0314be44e5c5efd8983f22599f52eb
Irfan Erol <erol.irfan@hotmail.com>
2013-07-12 22:40:54 +0000

    Modified web.js error

--- web.js
+++ web.js
@@ -5,9 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var file = fs.readFileSync('index.html', function(err,data) {
 	if(err) throw err;
-  	console.log(data);
+  	response.send(data);
   };
-  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
