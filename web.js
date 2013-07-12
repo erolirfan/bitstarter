5e6a898e027573b035183faef1dc81ebab09369e
Irfan Erol <erol.irfan@hotmail.com>
2013-07-12 22:35:36 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var file = fs.readFileSync('index.html', function(err,data) {
+	if(err) throw err;
+  	console.log(data);
+  };
   response.send('Hello World 2!');
 });
 
