0f121a10a7f37e450ff3a910afa99983b5641a06
Irfan Erol <erol.irfan@hotmail.com>
2013-07-13 23:38:32 +0000

    Modified port

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 	var fileText = fileBuffer.toString();
 	response.send(fileText);
 });
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
